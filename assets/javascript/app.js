bf9a9fa5bb62aa6ade96635f44694d9629fe8477
Ulises <uli.atx@gmail.com>
2019-05-29 21:37:09 -0500

    code commenting and clearing radio button check

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//Quiz pool. Now now, no cheating!
 var pool = {
     1: {
         q: "In Hercules, Hades promised not to harm Megara provided that Hercules give up his strength. How long did he have to agree to give up his strength for?",
@@ -91,20 +92,20 @@ var pool = {
     }
 
 }
-
+//Variable Declarations
 var choice = "";
 var numCorrect = 0;
 var numWrong = 0;
-var countDownTimer = 30;
+var countDownTimer = 10;
 var intervalId;
 var position = 1;
 var delay;
-
+//This function handles restarting the game by resetting the stats and bringing one back to the first question
 function restart() {
     choice = "";
     numCorrect = 0;
     numWrong = 0;
-    countDownTimer = 30;
+    countDownTimer = 10;
     intervalId;
     position = 1;
     delay;
@@ -120,7 +121,7 @@ function restart() {
     $("input:radio").show();
     $(".submit").show();
 }
-
+//This function decreases the timer and also handles what happens when the player runs out of time. The timeUp function increases the amount of wrong answers.
 function decrease() {
 
     if (countDownTimer <= 0) {
@@ -134,9 +135,10 @@ function decrease() {
     countDownTimer--;
 
 }
-
+//The next function handles what happens once the player goes through all of the questions.
+//It shows the stats at the end and shows the restart button.
+//This button will also reset the timer and move to the next question if the quiz is not over. 
 function next() {
-    
     
     if (position === 10){
         $(".submit").hide();
@@ -149,8 +151,9 @@ function next() {
     
     }
     else {
+    $("input:radio").prop( "checked", false );
     position++;
-    countDownTimer = 30;
+    countDownTimer = 10;
     intervalId = setInterval(decrease, 1000);
 
     displayQuestion();
@@ -159,17 +162,19 @@ function next() {
     $("#message").hide();
     }
 }
-
+//Add one to the number right and let the player know they got it.
 function rightAnswer() {
     $("#message").text("You got it!");
     $("#message").show();
     numCorrect++;
-}
 
+}
+//Add one to the number wrong and let the player know they didn't get it.
 function wrongAnswer() {
     numWrong++;
     $("#message").text("Shucks.. The answer was: " + pool[position].a)
     $("#message").show();
+
 }
 
 function timeUp() {
@@ -181,7 +186,8 @@ function timeUp() {
 function hideAnswer() {
     $("#message").hide();
 }
-
+//Both the hide and display question get called when moving in and out of different stages throughout the game.
+//The question will hide if a message is being displayed for example.
 function hideQuestion() {
 
     $("#question").hide();
@@ -195,7 +201,7 @@ function hideQuestion() {
     $(".submit").hide();
 
 }
-
+//After a message is shown and another question is queued, the displayQuestion function grabs the data from the question pool and displays it in the container.
 function displayQuestion() {
 
     clearInterval(delay);
@@ -234,14 +240,14 @@ function playRound() {
 }
 
 $(document).ready(function() {
-
+    //This doesn't need to show until the end.
     $(".restart").hide();
-
+    //Call the restart function to reset the quiz
     $(".restart").on("click", function(event){
         event.preventDefault();
         restart();
     });
-
+    //Starts the quiz and displays the question as well as the radio buttons.
     $(".start").on("click", function(event){
         event.preventDefault();
         
@@ -255,7 +261,7 @@ $(document).ready(function() {
         $(".submit").show();
     
     });
-
+    //Submits the input from the player and the logic will determine whether it was correct or not.
     $("input[type='submit']").click(function(event){
         event.preventDefault();
         choice = pool[position][$("input[type='radio']:checked").val()];
